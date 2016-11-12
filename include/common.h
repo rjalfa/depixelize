83e6d864543c33a39b70807a00835c0e5d0ba741
Rounaq <rounaqwl66@gmail.com>
2016-11-12 10:41:25 +0530

    Added COMMON for program-wide code

---
+++ include/common.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#include <cstdio>
+#include <tuple>
+#include <utility>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <stack>
+using namespace std;
+
+//DIRECTION MACROS
+#define TOP 1
+#define TOP_RIGHT 2
+#define TOP_LEFT 0
+#define LEFT 3
+#define RIGHT 4
+#define BOTTOM 6
+#define BOTTOM_RIGHT 7
+#define BOTTOM_LEFT 5
+
+extern int directions[8][2];
+
+
+template<class T,class V>
+ostream& operator<<(ostream& out, pair<T,V>& p)
+{
+	out << "{" << p.first << "," << p.second << "}";
+	return out;
+}
+
+template<class T>
+ostream& operator<<(ostream& out, vector<T> v)
+{
+	out << "[";
+	for(int i = 0 ; i < v.size() ; i++)
+	{
+		out << v[i];
+		if(i < v.size() - 1 ) out << ", ";
+	}
+	out << "]";
+	return out;
+}
+
+#endif
